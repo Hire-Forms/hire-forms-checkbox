41fded4c844398162ee8c2f108c1273b316cb7a7
Gijsjan <agijsbro@gmail.com>
2016-04-11 12:28:00 +0000

    Simplify checkbox

--- src/index.jsx
+++ src/index.jsx
@@ -23,27 +23,39 @@ class Checkbox extends React.Component {
 					{checked: this.props.value}
 				)}
 				onClick={this.handleClick.bind(this)}>
-				<svg viewBox="0 0 220 220">
-					<rect
-						fillOpacity="0"
-						height="190"
-						rx="20"
-						ry="20"
-						stroke="black"
-						strokeWidth="20"
-						width="190"
-						x="15"
-						y="15" />
-					<rect
-						height="200"
-						width="20"
-						x="180"
-						y="0" />
-					<rect
-						height="20"
-						width="200"
-						x="0"
-						y="180" />
+				<svg viewBox="0 0 240 220">
+					<g className="box">
+						<rect
+							fillOpacity="0"
+							height="190"
+							rx="20"
+							ry="20"
+							stroke="black"
+							strokeWidth="20"
+							width="190"
+							x="15"
+							y="15" />
+					</g>
+					<g className="checkmark">
+						<rect
+							fill="#00BFA5"
+							height="212"
+							rx="10"
+							ry="10"
+							transform="rotate(45)"
+							width="60"
+							x="150"
+							y="-130" />
+						<rect
+							fill="#00BFA5"
+							height="120"
+							rx="10"
+							ry="10"
+							transform="rotate(-45)"
+							width="60"
+							x="-90"
+							y="90" />
+					</g>
 				</svg>
 				{label}
 			</div>
